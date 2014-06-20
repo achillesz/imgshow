f0d3993afcc0d603028e35b7086a366fd307f841
zy <36891314@163.com>
2014-06-20 20:39:56 +0800

    first

---
+++ js/imgshow.js
@@ -0,0 +1,15 @@
+/**
+ * Created by wzy.wang on 2014/6/20.
+ */
+var ImgShow = function(obj){
+	this.imgs = obj.imgs || [];
+	this.alpha = obj.alpha;
+};
+// 初始化
+ImgShow.prototype.init = function(){
+
+};
+ImgShow.prototype.attachEvent = function(){};
+ImgShow.prototype.setPosition = function(){};
+ImgShow.prototype.doIt = function(){};
+
